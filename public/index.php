3df24ee1ae035ce97daa15c5512a30f576e8558a
Victor Sanchez <vsanchez1987@yahoo.com>
2015-02-19 08:58:58 -0800

    created header.php, footer.php, myCSS.css and added a form to the index page to search for resturants

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php require("../templates/header.php"); ?>
+
+
+<div class="container">
+	<div class="makeCenter">
+		<h1>Welcome to resturant finder </h1>
+		<p class="lead">
+			Use the form below to find the resturnant you want to find.
+			<br />
+			This will also expand to finding food trucks.
+		</p>
+	</div>
+</div>
+
+<form class="form-inline" method="get">
+	<div class="form-group" id="formDiv">
+		<label for="restName"> Resturant Name </label>
+		<input class="form-control" type="text" placeholder="Enter resturant name you want ot look for">
+		<input class="btn btn-default" type="submit" value="Search">
+	</div>
+</form>
+
+
+
+
+<?php require("../templates/footer.php");?>
---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/myCSS.css">
+	<title>Food Truck Tracker </title>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed"></button>
+			<a class="navbar-brand" href="#"> Food Truck </a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active">
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+		</div>
+		
+	</div>
+
+</nav>
